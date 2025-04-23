ce6f60a56d78465cebf43f4bfc2a4541535b0678
MandeepKaur7864 <mk6454785@gmail.com>
2025-04-23 15:51:36 +0530

    Added /createMovie and /updateMovie routes

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -35,12 +37,7 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 
 	id := params["id"]
 
-	for index, value := range movies {
-		if value.ID == id {
-			movies = append(movies[:index], movies[index+1:]...)
-			break
-		}
-	}
+	removeMovieByID(id)
 
 	json.NewEncoder(w).Encode(movies)
 }
@@ -57,6 +54,38 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func removeMovieByID(id string) {
+	for index, value := range movies {
+		if value.ID == id {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	id := params["id"]
+	removeMovieByID(id)
+
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = id
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
 }
 
 func main() {
@@ -77,6 +106,8 @@ func main() {
 	// routes
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Println("Server starting at port: 8000")
